583a7d686760abe181dae381f0201793863b5e4d
Evelynbotia <evelynbotia07@gmail.com>
2021-04-29 08:52:43 -0500

    login

---
+++ src/Modulo authentication/firebase.js
@@ -0,0 +1,16 @@
+
+import {} from '../lib/index.js';
+//<------Autenticación---->
+// Your web app's Firebase configuration
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCcwsf7v_1XVXk6XH8012kZm6sX1_bZ_UI",
+  authDomain: "funnypets-socialnetwork.firebaseapp.com",
+  projectId: "funnypets-socialnetwork",
+  storageBucket: "funnypets-socialnetwork.appspot.com",
+  messagingSenderId: "382607454154",
+  appId: "1:382607454154:web:98a68b56bc2391030de933"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
