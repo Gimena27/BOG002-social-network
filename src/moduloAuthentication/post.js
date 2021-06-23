33dfbf94897a70e2bd56b420ab7e79f1fb7923a8
Evelynbotia <evelynbotia07@gmail.com>
2021-06-23 13:26:51 -0500

    likes listos

--- src/moduloAuthentication/post.js
+++ src/moduloAuthentication/post.js
@@ -1,62 +1,86 @@
-import { deletePost, savePost, updateDislike, updateLike} from "./firebase.js";
+import { deletePost, savePost, updateDislike, updateLike } from "./firebase.js";
 import { formPost } from "./formPost.js";
-import { getUserId,setUser} from "./user.js";
-let likes=[];
+import { getUserId, setUser } from "./user.js";
+let likes = [];
 
 export function post(showPost) {
     const post = document.createElement("div");
-    const descripcion= document.createElement("h4");
-    const usuario= document.createElement("p");
-    const divBtns=document.createElement("div");
-    const btnBorrar=document.createElement("button");
-    const iconoBorrar=document.createElement("i");
-    const btnEdit=document.createElement("button");
-    const iconoEdit=document.createElement("i");
-    const btnLike=document.createElement("button");
-    const iconoLike=document.createElement("i");
-    descripcion.textContent= showPost.description;
+    const descripcion = document.createElement("h4");
+    const usuario = document.createElement("p");
+    const divBtns = document.createElement("div");
+    const btnBorrar = document.createElement("button");
+    const iconoBorrar = document.createElement("i");
+    const btnEdit = document.createElement("button");
+    const iconoEdit = document.createElement("i");
+    const btnLike = document.createElement("button");
+    const iconoLike = document.createElement("i");
 
-    post.className="info-post";
-    iconoBorrar.className="fas fa-trash-alt   btn-delete";
-    iconoBorrar.addEventListener("click",(e)=>{
+    descripcion.textContent = showPost.description;
+
+    post.className = "info-post";
+    iconoBorrar.className = "fas fa-trash-alt   btn-delete";
+    iconoBorrar.addEventListener("click", (e) => {
         deletePost(showPost.id).then(doc => {
             console.log(doc);
             return doc;
         });
     })
-    iconoEdit.className="far fa-edit   btn-editar";
-    iconoEdit.addEventListener("click",()=>{
+    iconoEdit.className = "far fa-edit   btn-editar";
+    iconoEdit.addEventListener("click", () => {
         console.log("se esta editando");
         formPost().editMode(showPost.id, showPost.description);
-        
+
     })
-   
-    iconoLike.className="far fa-heart   btn-like";
-    iconoLike.addEventListener("click",()=>{
+    // iconoLike.className="far fa-heart   btn-like";
+    let user = firebase.auth().currentUser;
+    console.log("usuario aaaaas", user.uid)
+    const likes = showPost.likes;
+    const miLike = likes.find(item => item === user.uid);
+    console.log("like", miLike)
+    if (miLike) {
+
+        iconoLike.className = "fas fa-heart"
+        // iconoLike.style.background ="red";
+
+    } else {
+        // debugger
+        iconoLike.className = "far fa-heart"
+        // iconoLike.style.background ="transparent";
+
+
+
+    }
+
+
+
+    // iconoLike.className="fas fa-heart   btn-like";
+    iconoLike.addEventListener("click", () => {
         console.log("like");
         let user = firebase.auth().currentUser;
-        console.log("usuario aaaaas",user.uid)
+        console.log("usuario aaaaas", user.uid)
         const likes = showPost.likes;
         const miLike = likes.find(item => item === user.uid);
-        console.log("like",miLike)
-        if(miLike){
-        
+        console.log("like", miLike)
+        if (miLike) {
+
             updateDislike(showPost.id, user.uid);
-            iconoLike.style.background ="transparent";
-         
-        }else{
+            // iconoLike.style.background ="red";
+
+        } else {
             // debugger
             updateLike(showPost.id, user.uid);
-            iconoDislike.style.background = "#D95F69";
-            
+            // iconoLike.style.background ="transparent";
+
+
+
         }
-       
-        
-        
+
+
+
     });
     usuario.innerHTML = showPost.likes.length === 0 ? '' : showPost.likes.length;
 
-    
+
     post.appendChild(descripcion);
     post.appendChild(usuario);
     post.appendChild(divBtns);
